3e00a51a55250623ad2aa3839e83d96e6937dfe8
Gioele Consani <gio-c90@hotmail.it>
2017-02-15 23:08:08 +0000

    Added and performed regression test - test working

--- boids/boids.py
+++ boids/boids.py
@@ -2,51 +2,60 @@ from matplotlib import pyplot as plt
 from matplotlib import animation
 import random
 import yaml
+import numpy as np
 
 config=yaml.load(open("config.yaml"))
 globals().update(config)
 
-boids_x=[random.uniform(xmin,xmax) for x in range(boids_count)]
-boids_y=[random.uniform(ymin,ymax) for x in range(boids_count)]
-boid_x_velocities=[random.uniform(vxmin,vxmax) for x in range(boids_count)]
-boid_y_velocities=[random.uniform(vymin,vymax) for x in range(boids_count)]
-boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
+lower_limits=np.array(p_limits[0:1])
+upper_limits=np.array(p_limits[2:3])
+width=upper_limits-lower_limits
 
-def update_boids(boids):
-	xs,ys,xvs,yvs=boids
+lower_v_limits=np.array(v_limits[0:2])
+upper_v_limits=np.array(v_limits[1:3])
+widthv=upper_v_limits-lower_v_limits
+
+positions=lower_limits[:,np.newaxis]+np.random.rand(2,boids_count)*width[:,np.newaxis]
+velocities=lower_v_limits[:,np.newaxis]+np.random.rand(2,boids_count)*widthv[:,np.newaxis]
+
+def update_boids(positions,velocities):
+	positions = positions
+	velocities = velocities
+
 # Fly towards the middle
-	for i in range(boids_count):
-		for j in range(boids_count):
-			xvs[i]=xvs[i]+(xs[j]-xs[i])*middle_strength/len(xs)
-			yvs[i]=yvs[i]+(ys[j]-ys[i])*middle_strength/len(xs)
-
+	middle = np.mean(positions,1)
+	direction_to_middle=positions-middle[:,np.newaxis]
+	velocities -= direction_to_middle*middle_strength
+	
 # Fly away from nearby boids
-
-	for i in range(boids_count):
-		for j in range(boids_count):
-			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < alert_distance:
-				xvs[i]=xvs[i]+(xs[i]-xs[j])
-				yvs[i]=yvs[i]+(ys[i]-ys[j])
-
+	separations = positions[:,np.newaxis,:]-positions[:,:,np.newaxis]
+	squared_displacements = separations*separations
+	square_distances = np.sum(squared_displacements,0)
+	far_away = square_distances > alert_distance
+	separations_if_close = np.copy(separations)
+	separations_if_close[0,:,:][far_away] = 0
+	separations_if_close[1,:,:][far_away] = 0
+	velocities += np.sum(separations_if_close, 1)
+	
 # Try to match speed with nearby boids
-	for i in range(boids_count):
-		for j in range(boids_count):
-			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < flocking_distance:
-				xvs[i]=xvs[i]+(xvs[j]-xvs[i])*flocking_strength/len(xs)
-				yvs[i]=yvs[i]+(yvs[j]-yvs[i])*flocking_strength/len(xs)
+	very_far = square_distances > flocking_distance
+	velocity_separation = velocities[:,np.newaxis,:] - velocities[:,:,np.newaxis]
+	velocity_separation_if_close = np.copy(velocity_separation)
+	velocity_separation_if_close[0,:,:][very_far] = 0
+	velocity_separation_if_close[1,:,:][very_far] = 0
+	velocities -= np.mean(velocity_separation_if_close, 1) * flocking_strength
+
 
 # Move according to velocities
-	for i in range(boids_count):
-		xs[i]=xs[i]+xvs[i]
-		ys[i]=ys[i]+yvs[i]
+	positions += velocities
 
 figure=plt.figure()
-axes=plt.axes(xlim=(-500,1500), ylim=(-500,1500))
-scatter=axes.scatter(boids[0],boids[1])
+axes=plt.axes(xlim=(-1500,1500), ylim=(-1500,1500))
+scatter=axes.scatter(positions[0],positions[1])
 
 def animate(frame):
-	update_boids(boids)
-	scatter.set_offsets(list(zip(boids_x,boids_y)))
+	update_boids(positions,velocities)
+	scatter.set_offsets(positions.transpose())
 
 anim = animation.FuncAnimation(figure,animate,frames=50,interval=50)
 if __name__ == "__main__":
