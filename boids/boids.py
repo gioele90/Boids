58f33df295ff9311fcdf1d8e6327e75a73258ac5
Gioele Consani <gio-c90@hotmail.it>
2017-02-15 23:54:05 +0000

    Split code in different functions

--- boids/boids.py
+++ boids/boids.py
@@ -19,18 +19,23 @@ positions=lower_limits[:,np.newaxis]+np.random.rand(2,boids_count)*width[:,np.ne
 velocities=lower_v_limits[:,np.newaxis]+np.random.rand(2,boids_count)*widthv[:,np.newaxis]
 
 def update_boids(positions,velocities):
-	positions = positions
-	velocities = velocities
-
-# Fly towards the middle
+	separations = positions[:,np.newaxis,:]-positions[:,:,np.newaxis]
+	squared_displacements = separations*separations
+	square_distances = np.sum(squared_displacements,0)
+	fly_to_middle(positions,velocities,middle_strength)
+	fly_away_from_boids(positions,velocities,alert_distance,separations,square_distances)
+	match_velocities(positions,velocities,flocking_distance,flocking_strength,square_distances)
+# Move according to velocities
+	positions += velocities
+	
+#Fly towards middle
+def fly_to_middle(positions,velocities,middle_strength):
 	middle = np.mean(positions,1)
 	direction_to_middle=positions-middle[:,np.newaxis]
 	velocities -= direction_to_middle*middle_strength
 	
 # Fly away from nearby boids
-	separations = positions[:,np.newaxis,:]-positions[:,:,np.newaxis]
-	squared_displacements = separations*separations
-	square_distances = np.sum(squared_displacements,0)
+def fly_away_from_boids(positions,velocities,alert_distance,separations,square_distances):
 	far_away = square_distances > alert_distance
 	separations_if_close = np.copy(separations)
 	separations_if_close[0,:,:][far_away] = 0
@@ -38,16 +43,13 @@ def update_boids(positions,velocities):
 	velocities += np.sum(separations_if_close, 1)
 	
 # Try to match speed with nearby boids
+def match_velocities(positions,velocities,flocking_distance,flocking_strength,square_distances):
 	very_far = square_distances > flocking_distance
 	velocity_separation = velocities[:,np.newaxis,:] - velocities[:,:,np.newaxis]
 	velocity_separation_if_close = np.copy(velocity_separation)
 	velocity_separation_if_close[0,:,:][very_far] = 0
 	velocity_separation_if_close[1,:,:][very_far] = 0
 	velocities -= np.mean(velocity_separation_if_close, 1) * flocking_strength
-
-
-# Move according to velocities
-	positions += velocities
 
 figure=plt.figure()
 axes=plt.axes(xlim=(-1500,1000), ylim=(-1500,1000))
