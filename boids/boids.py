cf739c0d99f60ce119b477a2912abbccb791d188
Gioele Consani <gio-c90@hotmail.it>
2017-02-16 21:01:16 +0000

    Replaced method arguments with class members

--- boids/boids.py
+++ boids/boids.py
@@ -39,37 +39,37 @@ class Boids(object):
 
 #Fly towards middle
 
-	def fly_to_middle(self,positions,velocities):
-		middle = np.mean(positions,1)
-		direction_to_middle=positions-middle[:,np.newaxis]
-		velocities -= direction_to_middle*self.middle_strength
+	def fly_to_middle(self):
+		middle = np.mean(self.positions,1)
+		direction_to_middle=self.positions-middle[:,np.newaxis]
+		self.velocities -= direction_to_middle*self.middle_strength
 
 # Fly away from nearby boids
-	def fly_away_from_boids(self,velocities,separations,square_distances):
+	def fly_away_from_boids(self,separations,square_distances):
 		far_away = square_distances > self.alert_distance
 		separations_if_close = np.copy(separations)
 		separations_if_close[0,:,:][far_away] = 0
 		separations_if_close[1,:,:][far_away] = 0
-		velocities += np.sum(separations_if_close, 1)
+		self.velocities += np.sum(separations_if_close, 1)
 
 # Try to match speed with nearby boids
-	def match_velocities(self,velocities,velocity_separation,square_distances):
+	def match_velocities(self,velocity_separation,square_distances):
 		very_far = square_distances > self.flocking_distance
 		velocity_separation_if_close = np.copy(velocity_separation)
 		velocity_separation_if_close[0,:,:][very_far] = 0
 		velocity_separation_if_close[1,:,:][very_far] = 0
-		velocities -= np.mean(velocity_separation_if_close, 1)*self.flocking_strength
+		self.velocities -= np.mean(velocity_separation_if_close, 1)*self.flocking_strength
 
-	def update_boids(self,positions,velocities):
-		separations=self.separations(positions)
-		velocity_separation=self.separations(velocities)
-		square_distances=self.squared_distances(positions)
-		self.fly_to_middle(positions,velocities)
-		self.fly_away_from_boids(velocities,separations,square_distances)
-		self.match_velocities(velocities,velocity_separation,square_distances)
+	def update_boids(self):
+		separations=self.separations(self.positions)
+		velocity_separation=self.separations(self.velocities)
+		square_distances=self.squared_distances(self.positions)
+		self.fly_to_middle()
+		self.fly_away_from_boids(separations,square_distances)
+		self.match_velocities(velocity_separation,square_distances)
 
 # Move according to velocities
-		positions += velocities
+		self.positions += self.velocities
 
 boids=Boids(boids_count,p_limits,v_limits,alert_distance,flocking_distance,flocking_strength,middle_strength)
 figure=plt.figure()
@@ -77,7 +77,7 @@ axes=plt.axes(xlim=(-1500,1000), ylim=(-1500,1000))
 scatter=axes.scatter(boids.positions[0],boids.positions[1])
 
 def animate(frame):
-	boids.update_boids(boids.positions,boids.velocities)
+	boids.update_boids()
 	scatter.set_offsets(boids.positions.transpose())
 
 anim = animation.FuncAnimation(figure,animate,frames=50,interval=50)
