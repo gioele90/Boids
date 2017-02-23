2a0539b0edb8e14451de0a4287c6caf6f61a9a98
Gioele Consani <gio-c90@hotmail.it>
2017-02-23 18:07:15 +0000

    Changed setup file

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ with open("README.txt", "rb") as f:
 	long_descr = f.read().decode("utf-8")
 setup(
 	name="boids",
-	packages=find_packages("project",exclude=["test"]),
+	packages=find_packages(exclude=["test"]),
 	scripts = ['scripts/boids'],
-	version="1.1",
+	version="1.2",
 	description="Run a simulation of birds flying",
 	long_description=long_descr,
 	author="Gioele Consani",
