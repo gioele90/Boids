2a3e9c9b346fda3b0ad38221f7f36276398a1dda
Gioele Consani <gio-c90@hotmail.it>
2017-02-24 10:54:05 +0000

    Changed setup file to exclude tests

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("README.txt", "rb") as f:
 	long_descr = f.read().decode("utf-8")
 setup(
 	name="boids",
-	packages=find_packages(exclude=["test"]),
+	packages=find_packages(exclude=['*test']),
 	scripts = ['scripts/boids'],
 	version="1.2",
 	description="Run a simulation of birds flying",
